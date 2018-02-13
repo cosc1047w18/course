762e6b4c35e5a048768fdd430241d3a540c0f6f7
pagzamaphone <anthony.pagnotta@gmail.com>
2018-02-13 18:02:29 -0500

    Create Lab4.java

---
+++ Lab4/Lab4.java
@@ -0,0 +1,92 @@
+package javaapplication;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Lab4 extends JFrame implements ActionListener {
+    private double[] a;
+    private JRadioButton divideByZeroButton;
+    
+    private JRadioButton badCastButton;
+    
+    private JRadioButton arrayBoundsButton;
+    
+    private JRadioButton nullPointerButton;
+    
+    private JRadioButton negSqrtButton;
+    
+    private JRadioButton overflowButton;
+    
+    private JRadioButton throwMyButton;
+    
+    public Lab4() {
+        
+        // Create a JPanel and GridLayout
+        JPanel p = new JPanel();
+        p.setLayout(new GridLayout(8, 1));
+        
+        // Create buttons and add them to the panel
+        ButtonGroup g = new ButtonGroup();
+        divideByZeroButton = addRadioButton("Divide by zero", g, p);
+        badCastButton = addRadioButton("Bad cast", g, p);
+        arrayBoundsButton = addRadioButton("Array bounds", g, p);
+        nullPointerButton = addRadioButton("Null pointer", g, p);
+        negSqrtButton = addRadioButton("sqrt(-1)", g, p);
+        overflowButton = addRadioButton("Overflow", g, p);
+        throwMyButton = addRadioButton("Throw My own Exception", g, p);
+        getContentPane().add(p);
+    }
+    
+    private JRadioButton addRadioButton(String s, ButtonGroup g, JPanel p) {
+        JRadioButton button = new JRadioButton(s, false);
+        button.addActionListener(this);
+        g.add(button);
+        p.add(button);
+        return button;
+    }
+    
+    // Trigger and catch various exceptions
+    public void actionPerformed(ActionEvent evt) {
+        try {
+            Object source = evt.getSource();
+            if (source == divideByZeroButton) {
+               // cause divideByZero exception
+               
+            } else if (source == badCastButton) {
+                // cause a bad cast exception
+                
+            } else if (source == arrayBoundsButton) {
+                //cause an array out of bounds exception
+                
+            } else if (source == nullPointerButton) {
+                //cause a null pointer exception
+                
+            } else if (source == negSqrtButton) {
+               //cause a negative Squareroot exception
+                
+            } else if (source == overflowButton) {
+                //cause an overflow exception
+                
+            } else if (source == throwMyButton) {
+                //create your own custom exception and throw it, and handle it
+            }
+        } catch (RuntimeException e) {
+            System.out.println("Caught RuntimeException: " + e);
+        } catch (Exception e) {
+            System.out.println("Caught Exception: " + e);
+        }
+    }
+    
+    public static void main(String[] args) {
+        JFrame frame = new Lab4();
+        frame.setSize(200, 200);
+        frame.addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        frame.setVisible(true);
+    }
+}
