361937b96b8fd20efd7e58476ea648edaeac3c60
pagzamaphone <anthony.pagnotta@gmail.com>
2018-03-13 00:20:50 -0400

    Create labSevenPartB.java

---
+++ Lab7/labSevenPartB.java
@@ -0,0 +1,25 @@
+package javaapplication2;
+
+
+public class labSevenPartB {
+
+    public static void main(String[] args) {
+
+        Integer[] list = new Integer[10];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = i;
+        }
+
+        System.out.println("Max = " + max(list));
+    }
+
+    public static <E extends Comparable<E>> E max(E[] list) {
+    
+    // YOUR CODE SHOULD GO HERE //
+    
+    
+    
+    
+
+    }
+}
