2c9dec7d174cc056c3745b4d55e7dca922bc94a4
pagzamaphone <anthony.pagnotta@gmail.com>
2018-03-13 00:18:00 -0400

    Create labSevenPartA.java

---
+++ Lab7/labSevenPartA.java
@@ -0,0 +1,29 @@
+package javaapplication;
+
+import java.util.ArrayList;
+
+public class labSevenPartA {
+
+    public static void main(String[] args) {
+
+        ArrayList<String> strings = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            strings.add("Hello");
+        }
+
+        strings = removeDuplicates(strings);
+        for (String s : strings) {
+            System.out.println(s);
+        }
+    }
+
+    public static <E> ArrayList<E> removeDuplicates(ArrayList<E> list) {
+
+      // YOUR CODE SHOULD GO HERE//
+      
+      
+      
+      
+      
+    }
+}
