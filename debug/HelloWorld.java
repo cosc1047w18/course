7f0b3bb3c973cc4c07dcb91182395dd7db1b00a4
pagzamaphone <anthony.pagnotta@gmail.com>
2018-03-27 04:06:37 -0400

    Create HelloWorld.java

---
+++ debug/HelloWorld.java
@@ -0,0 +1,12 @@
+package debugexample;
+
+public class HelloWorld {
+
+	public static void main (String [] args)
+	{
+		//set a breakpoint on the following line
+		String hello = "Hello World";
+		//set a second breakpoint on the following line
+		System.out.println (hello);
+	}
+}
