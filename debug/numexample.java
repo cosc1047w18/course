5a02cc979380e989c8d54e3139d06fdb377c5775
pagzamaphone <anthony.pagnotta@gmail.com>
2018-03-27 08:38:07 -0400

    Create numexample.java

---
+++ debug/numexample.java
@@ -0,0 +1,16 @@
+package debugexample;
+
+public class numexample {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int z = 3;
+
+		x++;
+		y += 1;
+		z *= 2;
+
+		System.out.println("x:" + x + "y:" + y + "z:" + z);
+	}
+}
